ebabeeee03fa0ef5ff7e61677739907207949f5e
samuel <samuel>
2020-04-24 01:18:49 +0200

    e2ee started

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { String, ObjectId, Boolean, Date } = mongoose.Schema.Types;
 module.exports = User = mongoose.model(
 	"User",
-	{ role: String, username: String, password: String, nickname: String },
+	{ role: String, username: String, password: String, nickname: String, publicKey: String },
 	"users"
 );
 // Teams werden in Teams.users abgespeichert
